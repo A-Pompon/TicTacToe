2795126c20546bd835d894fdf19c594b15218fcc
Arnaud POMPONIO <arnaudpomponio@yahoo.com>
2021-06-08 22:23:56 +0200

    [UI] - Grille en cours

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//Affichage des cases
+// var grille = document.getElementById("jeu")
+
+// var cells = [];
+// for (var i = 0; i < 9; i++) {
+//   cells.push(document.getElementById(i));
+// }
+
+// cells[0].style.borderTop="none";
+// cells[0].style.borderLeft="none";
+// cells[1].style.border="none";
+// cells[2].style.borderTop="none";
+// cells[2].style.borderRight="none";
+// cells[3].style.border="none";
+// cells[4].style.border="none";
+// cells[5].style.border="none";
+// cells[6].style.borderBottom="none";
+// cells[6].style.borderLeft="none";
+// cells[7].style.border="none";
+// cells[8].style.borderBottom="none";
+// cells[8].style.borderRight="none";
